d236c6090fdad0504f873b5af0ef0f12720782f2
Yash45-MJ <yashbhite45@gmail.com>
2024-07-11 15:38:58 +0530

    Create Calc_Stress_Strain.py

---
+++ Calc_Stress_Strain.py
@@ -0,0 +1,13 @@
+force = int(input("Enter the force: "))
+length = int(input("Enter the length: "))
+breath = int(input("Enter the breath: "))
+area = length*breath
+stress = force/area
+print(stress) 
+
+
+
+original_length = int(input("Enter the length: "))
+change_length = int(input("Enter the changed length: "))
+strain = change_length/original_length
+print(strain)
